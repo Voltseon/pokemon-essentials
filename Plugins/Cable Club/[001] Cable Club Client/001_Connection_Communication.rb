46ee69979c047603a98b631b9b9e37f1ee394790
Voltseon <voltseon@gmail.com>
2023-01-07 14:20:24 +0100

    Update 001_Connection_Communication.rb

--- Plugins/Cable Club/[001] Cable Club Client/001_Connection_Communication.rb
+++ Plugins/Cable Club/[001] Cable Club Client/001_Connection_Communication.rb
@@ -30,7 +30,7 @@ class Connection
     end
     # Process at most one record so that any control flow in the block doesn't cause us to lose records.
     if !@recv_records.empty?
-      if @recv_records.length > 20
+      if @recv_records.length > 5
         until @recv_records.empty?
           record = @recv_records.shift
         end
