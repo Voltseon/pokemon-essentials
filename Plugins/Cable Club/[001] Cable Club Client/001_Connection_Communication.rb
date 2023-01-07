fed95703a983a9a2abc5ac8604212210e2b8e7cb
Voltseon <voltseon@gmail.com>
2023-01-07 12:42:00 +0100

    Update 001_Connection_Communication.rb

--- Plugins/Cable Club/[001] Cable Club Client/001_Connection_Communication.rb
+++ Plugins/Cable Club/[001] Cable Club Client/001_Connection_Communication.rb
@@ -30,6 +30,7 @@ class Connection
     # Process at most one record so that any control flow in the block doesn't cause us to lose records.
     if !@recv_records.empty?
       record = @recv_records.shift
+      IO.flush
       if record.disconnect?
         reason = record.str() rescue "unknown error"
         raise Disconnected.new(reason)
