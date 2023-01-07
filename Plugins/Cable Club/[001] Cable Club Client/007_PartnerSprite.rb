65c92fed705cee834a5678c48e419baf87b73454
Voltseon <voltseon@gmail.com>
2023-01-07 17:00:18 +0100

    Update 007_PartnerSprite.rb

--- Plugins/Cable Club/[001] Cable Club Client/007_PartnerSprite.rb
+++ Plugins/Cable Club/[001] Cable Club Client/007_PartnerSprite.rb
@@ -14,7 +14,7 @@ class PartnerSprite < Sprite_Character
   end
 
   def character_name=(value)
-    @character.character_name = value
+    @character.character_name = value.to_string
   end
 
   def set_character(direction, pattern)
