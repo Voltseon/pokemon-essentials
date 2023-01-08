fc242a370b8b43143fe1ed9f386f27b6e1cd658d
Voltseon <voltseon@gmail.com>
2023-01-08 17:06:42 +0100

    as

--- Data/Scripts/022_Custom/998_Puzzles.rb
+++ Data/Scripts/022_Custom/998_Puzzles.rb
@@ -1,15 +1,19 @@
 def toggle_lamp_one
   $game_switches[102] = !$game_switches[102]
+  $game_map.need_refresh = true
 end
 
 def toggle_lamp_two
   $game_switches[103] = !$game_switches[103]
+  $game_map.need_refresh = true
 end
 
 def toggle_lamp_three
   $game_switches[104] = !$game_switches[104]
+  $game_map.need_refresh = true
 end
 
 def toggle_lamp_four
   $game_switches[105] = !$game_switches[105]
+  $game_map.need_refresh = true
 end
