9042cc2fbad227698edfaee4558bb5e895c524c3
Voltseon <voltseon@gmail.com>
2023-01-08 12:19:58 +0100

    Update 006_JoinSession.rb

--- Plugins/Cable Club/[001] Cable Club Client/006_JoinSession.rb
+++ Plugins/Cable Club/[001] Cable Club Client/006_JoinSession.rb
@@ -145,10 +145,10 @@ def update_leader
         break
       end
     end
-    x = (((record.int/10).to_f - $map_factory.getMap(partner_map).display_x) / Game_Map::X_SUBPIXELS).round + 1.5 * Game_Map::TILE_WIDTH
+    x = (((record.int/10).to_f - $map_factory.getMap($Partner_sprite.partner_map).display_x) / Game_Map::X_SUBPIXELS).round + 1.5 * Game_Map::TILE_WIDTH
     y = record.int
-    z = (((y/10).to_f - $map_factory.getMap(partner_map).display_y) / Game_Map::Y_SUBPIXELS).round + Game_Map::TILE_HEIGHT
-    y = (((y/10).to_f - $map_factory.getMap(partner_map).display_y) / Game_Map::Y_SUBPIXELS).round - Game_Map::TILE_HEIGHT / 2
+    z = (((y/10).to_f - $map_factory.getMap($Partner_sprite.partner_map).display_y) / Game_Map::Y_SUBPIXELS).round + Game_Map::TILE_HEIGHT
+    y = (((y/10).to_f - $map_factory.getMap($Partner_sprite.partner_map).display_y) / Game_Map::Y_SUBPIXELS).round - Game_Map::TILE_HEIGHT / 2
     x += record.int
     y += record.int
     $Partner_sprite.x = x
