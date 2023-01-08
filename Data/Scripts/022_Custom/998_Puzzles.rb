0b062560291c9f25c7520c1f434a24282e22f4cb
Voltseon <voltseon@gmail.com>
2023-01-08 17:00:48 +0100

    aa

---
+++ Data/Scripts/022_Custom/998_Puzzles.rb
@@ -0,0 +1,15 @@
+def toggle_lamp_one
+  $game_switches[102] = !$game_switches[102]
+end
+
+def toggle_lamp_two
+  $game_switches[103] = !$game_switches[103]
+end
+
+def toggle_lamp_three
+  $game_switches[104] = !$game_switches[104]
+end
+
+def toggle_lamp_four
+  $game_switches[105] = !$game_switches[105]
+end
