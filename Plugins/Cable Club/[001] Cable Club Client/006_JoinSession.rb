7945b3557c8425eb0d9b80e50e9e7184b8d930b2
Voltseon <voltseon@gmail.com>
2023-01-09 08:47:37 +0100

    sda

--- Plugins/Cable Club/[001] Cable Club Client/006_JoinSession.rb
+++ Plugins/Cable Club/[001] Cable Club Client/006_JoinSession.rb
@@ -164,10 +164,10 @@ def update_leader
       $Partner.visible = true
     end
 =end
-    x = (((record.int/10).to_f - $map_factory.getMap($Partner.partner_map,false).display_x) / Game_Map::X_SUBPIXELS).round + 1.5 * Game_Map::TILE_WIDTH
+    x = (((record.int/10).to_f - $game_map.display_x) / Game_Map::X_SUBPIXELS).round + 1.5 * Game_Map::TILE_WIDTH
     y = record.int
-    z = (((y/10).to_f - $map_factory.getMap($Partner.partner_map,false).display_y) / Game_Map::Y_SUBPIXELS).round + Game_Map::TILE_HEIGHT
-    y = (((y/10).to_f - $map_factory.getMap($Partner.partner_map,false).display_y) / Game_Map::Y_SUBPIXELS).round - Game_Map::TILE_HEIGHT / 2
+    z = (((y/10).to_f - $game_map.display_y) / Game_Map::Y_SUBPIXELS).round + Game_Map::TILE_HEIGHT
+    y = (((y/10).to_f - $game_map.display_y) / Game_Map::Y_SUBPIXELS).round - Game_Map::TILE_HEIGHT / 2
     x += record.int
     y += record.int
     $Partner.x = x
